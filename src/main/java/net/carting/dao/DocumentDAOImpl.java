d00d04ab7ad77f7941c47e2c44e79493539fb882
Sergey Manson <manson.serg@gmail.com>
2014-07-30 01:09:25 +0300

    Fixed deleting documents.

--- src/main/java/net/carting/dao/DocumentDAOImpl.java
+++ src/main/java/net/carting/dao/DocumentDAOImpl.java
@@ -1,12 +1,14 @@
 package net.carting.dao;
 
 import net.carting.domain.Document;
+import net.carting.domain.File;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public class DocumentDAOImpl implements DocumentDAO {
@@ -41,26 +43,40 @@ public class DocumentDAOImpl implements DocumentDAO {
 
     @Override
     public void deleteDocument(Document document) {
-        Query query = entityManager.createQuery(
-                "DELETE FROM Document c WHERE c.id = :id");
-        query.setParameter("id", document.getId());
-        query.executeUpdate();
+            String sql = "DELETE FROM racer_document WHERE document_id= :id";
+            Query query = entityManager.createNativeQuery(sql)
+                    .setParameter("id", document.getId());
+            query.executeUpdate();
+
+            query = entityManager.createNativeQuery(
+                    "DELETE FROM files WHERE document_id = :id");
+            query.setParameter("id", document.getId());
+            query.executeUpdate();
+
+            query = entityManager.createNativeQuery(
+                    "DELETE FROM documents WHERE id = :id");
+            query.setParameter("id", document.getId());
+            query.executeUpdate();
     }
 
     @Override
     public void deleteDocumentFromRacerByRacerIdAndDocumentId(int documentId,
                                                               int racerId) {
-        String sql = "DELETE FROM racer_document WHERE document_id= :documentId AND racer_id= :racerId";
-        Query query = entityManager.createQuery(sql)
-                .setParameter("documentId", documentId)
-                .setParameter("racerId", racerId);
-        query.executeUpdate();
+        try {
+            String sql = "DELETE FROM racer_document WHERE document_id= :documentId AND racer_id= :racerId";
+            Query query = entityManager.createNativeQuery(sql)
+                    .setParameter("documentId", documentId)
+                    .setParameter("racerId", racerId);
+            query.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public boolean isRacerOwnerOfDocument(int documentId) {
         String sql = "SELECT racer_id FROM racer_document WHERE document_id= :documentId  ";
-        Query query = entityManager.createQuery(sql).setParameter("documentId", documentId);
+        Query query = entityManager.createNativeQuery(sql).setParameter("documentId", documentId);
         boolean emptyList = query.getResultList().isEmpty();
         return !emptyList;
     }
