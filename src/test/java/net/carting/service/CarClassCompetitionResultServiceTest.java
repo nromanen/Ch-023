d1283892f406b2c0f6008fce0fd9992b7d987d75
mgalctc <kolio5991@gmail.com>
2014-09-16 11:36:14 +0300

    Added unit tests fow some services methods

--- src/test/java/net/carting/service/CarClassCompetitionResultServiceTest.java
+++ src/test/java/net/carting/service/CarClassCompetitionResultServiceTest.java
@@ -1,8 +1,23 @@
 package net.carting.service;
 
+import static junit.framework.Assert.assertEquals;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import net.carting.dao.CarClassCompetitionResultDAO;
 import net.carting.domain.CarClassCompetition;
 import net.carting.domain.CarClassCompetitionResult;
+import net.carting.domain.Competition;
+import net.carting.domain.Race;
+import net.carting.domain.RaceResult;
+import net.carting.domain.RacerCarClassCompetitionNumber;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,12 +25,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static junit.framework.Assert.assertEquals;
-import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CarClassCompetitionResultServiceTest {
@@ -25,7 +34,15 @@ public class CarClassCompetitionResultServiceTest {
 
     @Mock
     private CarClassCompetitionResultDAO carClassCompetitionResultDAO;
-
+    
+    @Mock
+    private RacerCarClassCompetitionNumberService racerCarClassCompetitionNumberService;
+    
+    @Mock
+    private CompetitionService competitionService;
+    @Mock
+    private RaceResultService raceResultService;
+    
     @Before
     public void init() {
         MockitoAnnotations.initMocks(this);
@@ -94,6 +111,95 @@ public class CarClassCompetitionResultServiceTest {
         carClassCompetitionResults.add(result2);
         when(carClassCompetitionResultDAO.getCarClassCompetitionResultsOrderedByPoints(carClassCompetition)).thenReturn(carClassCompetitionResults);
         assertEquals("Expected 2 results", 2, (carClassCompetitionResultServiceImpl.getCarClassCompetitionResultsOrderedByPoints(carClassCompetition).size()));
+    }
+    
+    @Test
+    public void setAbsoluteResultsTest() {
+        Race race = new Race();
+        race.setRaceNumber(1);
+        CarClassCompetition carClassCompetition = new CarClassCompetition();
+        carClassCompetition.setId(112);
+        Competition competition = new Competition();
+        competition.setEnabled(false);
+        carClassCompetition.setCompetition(competition);
+        RacerCarClassCompetitionNumber racerCarClassCompetitionNumber = new RacerCarClassCompetitionNumber();
+        racerCarClassCompetitionNumber.setNumberInCompetition(12);
+        CarClassCompetitionResult carClassCompetitionResult = new CarClassCompetitionResult();
+        carClassCompetitionResult.setAbsolutePlace(0);
+        carClassCompetitionResult.setAbsolutePoints(0);
+        carClassCompetitionResult.setRace2points(0);
+        carClassCompetitionResult.setRacerCarClassCompetitionNumber(racerCarClassCompetitionNumber);
+        List<RaceResult> raceResults = new ArrayList<RaceResult>();
+        RaceResult raceResult = new RaceResult();
+        raceResult.setCarNumber(12);
+        raceResult.setPlace(1);
+        raceResult.setPoints(100);
+        raceResults.add(raceResult);
+        
+        List<RacerCarClassCompetitionNumber> carClassCompetitionNumbers = new ArrayList<RacerCarClassCompetitionNumber>();
+        carClassCompetitionNumbers.add(racerCarClassCompetitionNumber);
+        
+        for (RaceResult raceRes : raceResults) {
+            if (raceRes.getCarNumber() == racerCarClassCompetitionNumber
+                    .getNumberInCompetition()) {
+                carClassCompetitionResult.setAbsolutePlace(raceRes
+                        .getPlace());
+                carClassCompetitionResult.setAbsolutePoints(raceRes
+                        .getPoints());
+            }
+        }
+        
+        when(racerCarClassCompetitionNumberService.getRacerCarClassCompetitionNumbersByCarClassCompetitionId(carClassCompetition.getId())).thenReturn(carClassCompetitionNumbers);
+        doNothing().when(competitionService).updateCompetition(competition);
+        doNothing().when(carClassCompetitionResultDAO).addCarClassCompetitionResult(carClassCompetitionResult);
+        when(raceResultService.getRaceResultsByRace(race)).thenReturn(raceResults);
+        
+        carClassCompetitionResultServiceImpl.setAbsoluteResults(carClassCompetition, race);
+        verify(carClassCompetitionResultDAO, times(1)).addCarClassCompetitionResult(carClassCompetitionResult);
+        verify(competitionService, times(1)).updateCompetition(competition);
+        verify(racerCarClassCompetitionNumberService, times(1)).getRacerCarClassCompetitionNumbersByCarClassCompetitionId(carClassCompetition.getId());
+        verify(raceResultService, times(1)).getRaceResultsByRace(race);
+        verifyNoMoreInteractions(racerCarClassCompetitionNumberService, competitionService, carClassCompetitionResultDAO, raceResultService);
+        
+        
+    }
+    
+    @Test
+    public void setAbsoluteResultsTest2() {
+        Race race = new Race();
+        race.setRaceNumber(2);
+        CarClassCompetition carClassCompetition = new CarClassCompetition();
+        carClassCompetition.setId(112);
+        List<CarClassCompetitionResult> absoluteResultsList = new ArrayList<CarClassCompetitionResult>();
+        List<RaceResult> raceResults = new ArrayList<RaceResult>();
+        
+        Competition competition = new Competition();
+        competition.setEnabled(false);
+        carClassCompetition.setCompetition(competition);
+        RacerCarClassCompetitionNumber racerCarClassCompetitionNumber = new RacerCarClassCompetitionNumber();
+        racerCarClassCompetitionNumber.setNumberInCompetition(12);
+        CarClassCompetitionResult carClassCompetitionResult = new CarClassCompetitionResult();
+        carClassCompetitionResult.setAbsolutePlace(0);
+        carClassCompetitionResult.setAbsolutePoints(0);
+        carClassCompetitionResult.setRace2points(0);
+        carClassCompetitionResult.setRacerCarClassCompetitionNumber(racerCarClassCompetitionNumber);
+        
+        RaceResult raceResult = new RaceResult();
+        raceResult.setCarNumber(12);
+        raceResult.setPlace(1);
+        raceResult.setPoints(100);
+        raceResults.add(raceResult);
+        absoluteResultsList.add(carClassCompetitionResult);
+        
+        when(raceResultService.getRaceResultsByRace(race)).thenReturn(raceResults);
+        doNothing().when(carClassCompetitionResultDAO).updateCarClassCompetitionResult(carClassCompetitionResult);
+        
+        
+        carClassCompetitionResultServiceImpl.setAbsoluteResults(carClassCompetition, race);
+        verify(raceResultService, times(1)).getRaceResultsByRace(race);
+        verifyNoMoreInteractions(raceResultService);
+        
+        
     }
 
 }
